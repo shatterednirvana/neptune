cd8bd585e3290de78b9e209a9812eda52f73a1f9
Chris Bunch <shatterednirvana@gmail.com>
2012-05-23 23:23:43 -0700

    added documentation on exodus methods, and regenerated rdoc/coverage

--- lib/exodus.rb
+++ lib/exodus.rb
@@ -5,6 +5,11 @@
 require 'custom_exceptions'
 
 
+# Exodus provides further improvements to Babel. Instead of making users tell
+# us what compute, storage, and queue services they want to use (required for
+# babel calls), Exodus will automatically handle this for us. Callers need
+# to specify what clouds their job can run over, and Exodus will automatically
+# select the best cloud for their job and run it there.
 def exodus(jobs)
   if jobs.class == Hash
     jobs = [jobs]
@@ -18,16 +23,20 @@ def exodus(jobs)
   jobs.each { |job|
     ExodusHelper.ensure_all_params_are_present(job)
   }
-
 end
 
 
+# This module provides convenience functions for exodus(), to avoid cluttering
+# up Object or Kernel's namespace.
 module ExodusHelper
 
 
+  # A list of clouds that users can run tasks on via Exodus.
   SUPPORTED_CLOUDS = %w{amazon-ec2 microsoft-azure google-app-engine}
 
 
+  # Given an Array of jobs to run, ensures that they are all Hashes, the
+  # standard format for Neptune jobs.
   def self.ensure_all_jobs_are_hashes(jobs)
     jobs.each { |job|
       if job.class != Hash
@@ -38,6 +47,8 @@ module ExodusHelper
   end
 
 
+  # Given an Exodus job, validates its parameters, raising a 
+  # BadConfigurationException for any missing params.
   def self.ensure_all_params_are_present(job)
     if job[:clouds_to_use].nil?
       raise BadConfigurationException.new(":clouds_to_use was not specified")
@@ -48,6 +59,10 @@ module ExodusHelper
   end
 
 
+  # Given a single Exodus job, checks to make sure it has either an Array
+  # of Strings or a single String listing the clouds that a given task can
+  # run on. Raises a BadConfigurationException if :clouds_to_use is not in
+  # the right format.
   def self.convert_clouds_to_use_to_array(job)
     clouds_class = job[:clouds_to_use].class
     if clouds_class == String
@@ -66,9 +81,14 @@ module ExodusHelper
   end
 
 
+  # Given a single Exodus job, checks to make sure that we can actually run
+  # it in this version of Neptune, and that the user has given us all the 
+  # credentials needed to use that cloud.
   def self.validate_clouds_to_use(job)
     job[:clouds_to_use].each { |cloud|
-      if !SUPPORTED_CLOUDS.include?(cloud)
+      if SUPPORTED_CLOUDS.include?(cloud)
+        #self.ensure_credentials_are_present_for_cloud(job)
+      else
         raise BadConfigurationException.new("#{cloud} was specified as in " +
           ":clouds_to_use, which is not a supported cloud.")
       end
