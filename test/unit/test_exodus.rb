a02443c87561bb4affb969aa4dd3ae00ef992113
Chris Bunch <shatterednirvana@gmail.com>
2012-05-23 21:36:21 -0700

    starting work on exodus, a variant of babel that does not require the user to tell us which cloud compute, storage, and queue that they want to run their task on

---
+++ test/unit/test_exodus.rb
@@ -0,0 +1,15 @@
+# Programmer: Chris Bunch (cgb@cs.ucsb.edu)
+
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "..", "lib")
+#require 'exodus'
+
+
+require 'rubygems'
+require 'flexmock/test_unit'
+
+
+class TestExodus < Test::Unit::TestCase
+  def test_nothing
+  end
+end
--- test/unit/ts_all.rb
+++ test/unit/ts_all.rb
@@ -7,6 +7,7 @@ require 'flexmock/test_unit'
 $:.unshift File.join(File.dirname(__FILE__))
 require 'test_babel'
 require 'test_common_functions'
+require 'test_exodus'
 require 'test_neptune'
 require 'test_neptune_manager_client'
 require 'test_task_info'
