6b56716bbeb2da927742f4bd6e4062194f14111c
Chris Bunch <shatterednirvana@gmail.com>
2012-10-11 15:30:13 -0700

    rearranged custom exceptions into alphabetical order

--- lib/custom_exceptions.rb
+++ lib/custom_exceptions.rb
@@ -1,10 +1,5 @@
 # Programmer: Chris Bunch
 
-# A special class of exceptions that are thrown whenever the NeptuneManager
-# experiences an unexpected result.
-class NeptuneManagerException < Exception
-end
-
 
 # A class of exceptions that are thrown when the user tries to run a Neptune
 # job but fails to give us the correct parameters to do so.
@@ -15,4 +10,10 @@ end
 # An exception that is thrown whenever the user specifies a file to use
 # that does not exist.
 class FileNotFoundException < Exception
+end
+
+
+# A special class of exceptions that are thrown whenever the NeptuneManager
+# experiences an unexpected result.
+class NeptuneManagerException < Exception
 end
